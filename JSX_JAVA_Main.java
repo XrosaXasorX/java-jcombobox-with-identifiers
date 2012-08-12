e10124f13df6c5ec7c2acfdf9be354bc2e2d7cb0
roberto.sagoleo@gmail.com <roberto.sagoleo@gmail.com@b69facf8-0bea-e982-9410-2f167c351bf5>
2012-08-12 21:21:42 +0000

    V1.0 created.

---
+++ JSX_JAVA_Main.java
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////
+//
+// JESAX.NET [ http://www.jesax.net/ ] 
+//  
+// JAVA/script Environmental Subroutines for Accessible XML 
+//  
+// BoF :: "JSX_JAVA_Main.java"
+// 
+// (C) CopyRight JESAX.net / SagoSoft.it [1984 - 2012] 
+// 
+// Last update: 2012/07/19 - 12:34 
+//
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+public class JSX_JAVA_Main 
+	{
+
+	public static void main(String[] args) 
+		{
+		System.out.println( "JSX:: JAVA 'JComboBox-IDentifier' starting..." );
+		JSX_JAVA_JFrame_IDentifier l_jsx_jframe_identifier;
+		l_jsx_jframe_identifier = new JSX_JAVA_JFrame_IDentifier( "JSX.JAVA     >>>>> JComboBox-IDentifier <<<<<" );
+		l_jsx_jframe_identifier.setVisible(true);
+		}//main
+
+	}//class
+///////////////////////////////////////////////////////////////////
+	
+///////////////////////////////////////////////////////////////////
+// EoF :: "JSX_JAVA_Main.java"
+///////////////////////////////////////////////////////////////////
+
